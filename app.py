db5b329b4900bacfae570c5c3f601285cfb1740d
tong4627 <yeutong00@gmail.com>
2023-08-30 02:28:52 +0800

    deploy

--- app.py
+++ app.py
@@ -56,4 +56,5 @@ def generate_tale(words):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
